0e3fdc020f6ab174c0c43a49cfa86c89a27b0640
Luke Vo <datvm@outlook.com>
2018-07-07 01:16:36 +0700

    Added IgnoreAttribute

---
+++ ServiceSharp/IgnoreAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceSharp
+{
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class IgnoreAttribute : Attribute
+    {
+    }
+
+}
